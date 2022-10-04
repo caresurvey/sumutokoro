fd6a6cf03a17ff0d55ad51255a4a79e7d23bccef
caresurvey <develop@caresurvey.co.jp>
2022-10-04 16:20:21 +0900

    deployer

--- deploy.php
+++ deploy.php
@@ -17,50 +17,27 @@ add('shared_files', ['.env']);
 add('shared_dirs', ['vendor']);
 add('writable_dirs', ['bootstrap/cache', 'storage']);
 
-// Hosts
-import('inventory.yaml');
 
 
-/*
-task('aaaproduction', function(){
-    // 本番環境のデプロイ先サーバのhost
-    host('sumutokoro3.sakura.ne.jp')
-        ->setRemoteUser('sumutokoro3')
-        ->setDeployPath('~/deploy')
-        //->setIdentifyFile('~/.ss')
-        ->set('branch', 'release');
-
-});
-*/
-
-
-// 事前に.dev.productionなどを(.env.ステージ名の)ファイルを準備しておく
-before('deploy:shared', 'upload-env');
-task('upload-env', function () {
-    $stage = get('stage');
-    $src = ".env.${stage}";
-    $deployPath = get('deploy_path');
-    $sharedPath = "${deployPath}/shared";
-    upload(__DIR__ . "/" . $src, "${sharedPath}/.env");
-});
-
-after('deploy:failed', 'deploy:unlock');
 
 /**
- * プロダクション環境へデプロイ
+ * Host設定
  */
-task('deploy:production', [
-    'deploy:info',
+host('production')
+    ->setHostname('sumutokoro3.sakura.ne.jp')
+    ->setRemoteUser('sumutokoro3')
+    ->setIdentityFile('sumutokoro3')
+    ->setDeployPath('~/deploy/production/sumutokoro_2022');
+
+
+/**
+ * 実行Task設定
+ */
+/*
+task('production', [
     'deploy:prepare',
-    'deploy:release',
-    'rsync',
-    'deploy:setting_production',
-    'deploy:after_setting',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:clear_paths',
+    'deploy:update_code',
+    'deploy:vendors',
     'deploy:symlink',
-    'cleanup',
-    'success',
-    'deploy:unlock',
 ]);
+*/
