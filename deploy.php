d44d61e94afe3d47566f2d4eb6fa2c5dfa8e3d55
caresurvey <develop@caresurvey.co.jp>
2022-10-04 19:06:27 +0900

    deployer

--- deploy.php
+++ deploy.php
@@ -9,15 +9,12 @@ set('ssh_multiplexing', true);
 set('git_tty', false); 
 
 // リポジトリ指定
-set('repository', 'git@github.com:caresurvey/sumutokoro.git');
+set('repository', 'git@github:caresurvey/sumutokoro.git');
 
-// 世代管理の件数
-set('keep_releases', 3);
-
+// 共有ファイルの設定
 add('shared_files', ['.env']);
 add('shared_dirs', ['vendor']);
 add('writable_dirs', ['bootstrap/cache', 'storage']);
-
 
 /**
  * Host設定
@@ -28,52 +25,27 @@ host('production')
     ->setPort(22)
     ->setIdentityFile('~/.ssh/secretkey')
     ->setDeployPath('~/deploy/production/sumutokoro_2022')
-    ->set('keep_releases', 5)
-    ->set('rsync_dest','{{release_path}}')
-    ->set('rsync',[
-        'exclude' => [
-            '.git',
-            '.github',
-            '.gitignore',
-            '.idea',
-            '.env',
-            '.env.dusk',
-            '.gitattributes',
-            '.styleci.yml',
-            '.editorconfig',
-            'artisan',
-            'deploy.php',
-            'package.json',
-            'phpunit.dusk.xml',
-            'phpunit.xml',
-            'server.php',
-            'servers.yml',
-            'assets',
-            'composer.lock',
-            'README.md',
-            'RoboFile.php',
-            'tests',
-            'system',
-            'docker-compose.yml',
-            'README.md'
-        ],
-        'exclude-file' => false,
-        'include'      => [],
-        'include-file' => false,
-        'filter'       => [],
-        'filter-file'  => false,
-        'filter-perdir'=> false,
-        'flags'        => 'rz', // Recursive, with compress
-        'options'      => ['delete'],
-        'timeout'      => 300,
-    ]
-);
+    ->set('keep_releases', 5);
 
-
-
+// Clone後の処理
 task('build', function () {
     run('cd {{release_path}}');
+    run('rm .gitignore');
+    run('rm docker-compose.yml');
+    run('rm phpunit.xml');
+    run('rm -R system');
 });
 
 after('deploy:failed', 'deploy:unlock');
-before('deploy:symlink', 'artisan:migrate');
+before('deploy:symlink');
+
+
+/*
+task('build', function () {
+    cd('{{release_path}}');
+    run('npm install');
+    run('npm run prod');
+});
+
+after('deploy:update_code', 'build');
+*/
