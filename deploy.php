7af72f75b33875bc3f22055a7c42cc327dd06e57
caresurvey <develop@caresurvey.co.jp>
2022-10-04 21:45:30 +0900

    deployer

--- deploy.php
+++ deploy.php
@@ -14,6 +14,8 @@ set('repository', 'git@github:caresurvey/sumutokoro.git');
 // 共有ファイルの設定
 add('shared_files', ['.env']);
 add('shared_dirs', ['vendor']);
+
+// 書き込みファイルの設定
 add('writable_dirs', ['bootstrap/cache', 'storage']);
 
 /**
@@ -30,10 +32,21 @@ host('production')
 // Deploy後の処理
 after('deploy', 'deploy:done');
 task('deploy:done', function () {
+    // 移動
     cd("{{release_path}}");
+
+    // 不要ファイルの削除
     run('rm .gitignore');
+    run('rm .env.dusk');
+    run('rm .env.testing');
     run('rm docker-compose.yml');
     run('rm phpunit.xml');
     run('rm -R system');
+
+    // シンボリックリンク追加
+    run('ln -s storage/app/photos public/photos');
+
+    // シンボリックリンク削除
+    run('unlink public/storage');
 });
 
